51bce5bed040adda89696f0e2732eee2ca6ac6f0
simicr <simradovan@gmail.com>
2022-11-04 22:03:00 +0100

    nasFilter.hs izmenjen

--- Vezbe1/nasFilter.hs
+++ Vezbe1/nasFilter.hs
@@ -4,4 +4,3 @@ filter' uslov [] = []
 filter' uslov (glava:rep) | (uslov glava) = glava:(filter' uslov rep)
                           | otherwise = filter' uslov  rep
 
-main = print (filter' (\ x-> (mod x 2) == 0) [1,2,3,4,5])
