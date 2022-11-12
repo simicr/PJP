93c0270d4351743804981c74503e7f7a0e645a5d
simicr <simradovan@gmail.com>
2022-11-12 14:47:40 +0100

    Implementacija K-means algoritma.

--- Kmeans.hs
+++ Kmeans.hs
@@ -1,19 +1,60 @@
-import System.Random
+{-
+Potrebno je napisati funkciju koja će da izvrši klasterizovanje (grupisanje) liste tačaka L
+u K klastera (grupa) pomoću K-Means algoritma. Ovaj algoritam se izvodi u više
+iteracija. Počev od nekih K centralnih tačaka (centri klastera), algoritam izvodi sledeće
+korake:
+- svakoj tački iz L pronađe najbližu od centralnih tačaka. Tačka pripada onom
+klasteru čijem centru je najbliža.
+- nakon što je svakoj tački iz L dodeljen klaster, računaju se nove centralne tačke.
+Novi centar svakog klastera računa se na osnovu svih tačaka iz L koji pripadaju
+tom klasteru. Centar klastera​ i​ u ​narednoj i​ teraciji računa se kao suma svih tačaka klastera ​i
+​u trenutnoj iteraciji podeljena sa brojem tačaka koji pripadaju tom klasteru.
+Algoritam se završava kada između 2 iteracije ne dođe do promene u klasterima. Za
+početne centre klastera uzeti K nasumičnih tačaka iz liste tačaka L.
+Glavna funkcija očekuje listu tačaka L (tačka = par Double-ova), kao i neko K koje
+predstavlja broj klastera na koji je potrebno podeliti listu tačaka L.Funkcija vraća listu
+parova oblika:
+( (Double, Double) , Int ) koji predstavljaju (tacku, broj klastera)
+-}
+
+
 type Tacka = (Double, Double)
 
+-- Vraca rastojanje izmedju dve tacke
 rastojanje:: Tacka -> Tacka -> Double
 rastojanje (x1,y1) (x2,y2) = sqrt(abs(x1-x2)*abs(x1-x2) +  abs(y1-y2)*abs(y1-y2))
 
-kmeans::[Tacka] -> Int -> [Int]
-kmeans input k = kmeans2 input k [] [1..k]
+-- Glavna funkcija
+kmeans::[Tacka] -> Int -> [(Tacka, Int)]
+kmeans input k = kmeans2 input k [] [1..k] (take k input) 
 
-kmeans2:: [Tacka] -> Int -> [Int] -> [Int] -> [Int]
-kmeans2 input k  preIter iter 
-                            | preIter == iter = iter
-                            | otherwise = kmeans2 input k iter (novoKlasterisanje centri input)
+-- Funkcija koja zapravo izracunava
+kmeans2:: [Tacka] -> Int -> [Int] -> [Int] -> [Tacka] -> [(Tacka, Int)]
+kmeans2 input k  preIter iter centri
+                            | preIter == iter = zip input iter
+                            | otherwise = kmeans2 input k iter noviIter noviCentri
     where 
-        centri = [(1.0,1.0)| x <- [1..k]] -- def funkciju koja u odnosu na iter izracuna nove centre 
+         noviIter = novoKlasterisanje centri input
+         noviCentri = recentriranje input noviIter k
+         
 
+-- Deo programa sa kojim cemo odrediti nove centre
+recentriranje::[Tacka] -> [Int] -> Int -> [Tacka]
+recentriranje input iter k = [( (sumaX parovi klaster)/(broj klaster iter), (sumaY parovi klaster)/(broj klaster iter) ) | klaster <- [1..k] ] 
+    where 
+        parovi = zip input iter
+
+-- Uzece listu parova (Tacka, Int) filtirace one koji pripadaju istom klasteru koji je dat i sumira X komponente.
+sumaX:: [(Tacka, Int)] -> Int -> Double
+sumaX parovi klaster = sum (map (fst . fst) (filter (\x -> (snd x) == klaster) parovi))
+-- Uzece listu parova (Tacka, Int) filtirace one koji pripadaju istom klasteru koji je dat i sumira Y komponente.
+sumaY:: [(Tacka, Int)] -> Int -> Double
+sumaY parovi klaster =  sum (map (snd . fst) (filter (\x -> (snd x) == klaster) parovi))
+-- Vraca koliko ima tacaka u istom klasteru, stim da se vrsi konverzija u Double.
+broj:: Int -> [Int] -> Double
+broj klaster iter = fromIntegral (length (filter (klaster ==) iter))
+
+-- Deo programa sa kojim cemo izracuniati kom klasteru pripada tacka 
 novoKlasterisanje::[Tacka] -> [Tacka] -> [Int]
 novoKlasterisanje centri input = map (najbliza centri 1 0 10000.0) input
 
